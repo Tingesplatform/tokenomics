89b624404c674ff9712cc18c23f166a7321900d1
Kirill Varlamov <kirill@ongrid.pro>
2018-11-27 18:35:56 +0300

    Add simple Bucket and Tap

--- test_tokenomics.py
+++ test_tokenomics.py
@@ -1,7 +1,7 @@
-from tokenomics import Account, ERC20Token
+from tokenomics import Account, ERC20Token, Bucket, Tap
 
 def test_mint_to_single_account():
-    alice = Account("Alice")
+    alice = Account()
     token = ERC20Token()
     assert token.total_supply == 0
     token.mint(alice, 100)
@@ -9,8 +9,8 @@ def test_mint_to_single_account():
     assert token.total_supply == 100
 
 def test_mint_and_transfer_all():
-    alice = Account("Alice")
-    bob = Account("Bob")
+    alice = Account()
+    bob = Account()
     token = ERC20Token()
     token.mint(alice, 100)
     token.transfer(alice, bob, 100)
@@ -19,8 +19,8 @@ def test_mint_and_transfer_all():
     assert token.total_supply == 100
 
 def test_mint_and_transfer_partial():
-    alice = Account("Alice")
-    bob = Account("Bob")
+    alice = Account()
+    bob = Account()
     token = ERC20Token()
     token.mint(alice, 100)
     token.transfer(alice, bob, 99)
@@ -33,3 +33,17 @@ def test_mint_and_transfer_partial():
     assert token.balance_of(alice) == 2
     assert token.balance_of(bob) == 98
     assert token.total_supply == 100
+
+def test_bucket_overflow():
+    token = ERC20Token()
+    of_bkt = Bucket(token=token)
+    bkt = Bucket(token=token, overflow_bkt=of_bkt)
+    token.mint(bkt, 1000000)
+    bkt.flush()
+    assert token.balance_of(bkt) == 1000
+    assert token.balance_of(of_bkt) == 1000000 - 1000
+    assert token.total_supply == 1000000
+    token.mint(bkt, 1000)
+    bkt.flush()
+    assert token.balance_of(of_bkt) == 1000000
+
--- tokenomics.py
+++ tokenomics.py
@@ -1,14 +1,19 @@
 """The tokenomics package for token economy modeling"""
+import os
+
 
 class Account:
     """Ethereum account"""
-    def __init__(self, addr: str):
-        self.addr = addr
+    def __init__(self, frm = None):
+        if frm:
+            self.owner = frm
+        self.addr = "0x"+os.urandom(20).hex()
 
 
-class ERC20Token:
+class ERC20Token(Account):
     """Basic ERC-20 token class with balances and totalSupply"""
     def __init__(self):
+        super().__init__()
         self.balances = {}
         self.total_supply = 0
 
@@ -30,3 +35,47 @@ class ERC20Token:
         if to.addr not in self.balances:
             self.balances[to.addr] = 0
         self.balances[to.addr] += tokens
+
+class Bucket(Account):
+    """The container of predefined volume storing raised funds"""
+    def __init__(self, token=None, max_volume = 1000, overflow_bkt = None):
+        super().__init__()
+        self.max_volume = max_volume
+        self.token = token
+        self.taps = [] # egress taps authorized to withdraw
+        self.overflow_bkt = overflow_bkt
+
+    def flush(self):
+        token_balance = self.token.balance_of(self)
+        if token_balance > self.max_volume:
+            self.token.transfer(self, self.overflow_bkt, token_balance - self.max_volume)
+
+    def set_overflow_bucket(self, overflow_bkt):
+        self.overflow_bkt = overflow_bkt
+
+    def add_tap(self, tap_addr):
+        pass
+
+    def del_tap(self, tap_addr):
+        pass
+
+
+class Tap(Account):
+    def __init__(self, upstream = None):
+        super().__init__()
+        self.upstream = upstream # upstream bucket
+
+    def close(self):
+        pass
+
+    def open(self):
+        pass
+
+    def set_rate(self, rate):
+        pass
+
+    def withdraw(self, amount):
+        pass
+
+    def withdraw_all(self):
+        pass
