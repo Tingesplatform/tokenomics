195b3f8fc3715c5a5fff4b5f444923c2eca3cbe4
Kirill Varlamov <kirill@ongrid.pro>
2018-11-28 19:38:04 +0300

    Improve tap and bucket

--- test_tokenomics.py
+++ test_tokenomics.py
@@ -1,4 +1,6 @@
-from tokenomics import Account, ERC20Token, Bucket, Tap
+from tokenomics import Account, ERC20Token, Bucket, Tap, Timer
+from datetime import timedelta
+import sys
 
 def test_mint_to_single_account():
     alice = Account()
@@ -7,6 +9,13 @@ def test_mint_to_single_account():
     token.mint(alice, 100)
     assert token.balance_of(alice) == 100
     assert token.total_supply == 100
+
+def test_timer():
+    timer = Timer()
+    oldtime = timer.time
+    timer.increment(100)
+    assert timer.time - oldtime == timedelta(seconds=100)
+
 
 def test_mint_and_transfer_all():
     alice = Account()
@@ -46,4 +55,98 @@ def test_bucket_overflow():
     token.mint(bkt, 1000)
     bkt.flush()
     assert token.balance_of(of_bkt) == 1000000
+
+def test_bucket_and_tap():
+    timer = Timer()
+    alice = Account()
+    bob = Account()
+    token = ERC20Token()
+    bkt = Bucket(token=token)
+    token.mint(bkt, 1000000)
+    assert token.balance_of(bkt) == 1000000
+    alices_tap = Tap(withdrawer=alice, upstream=bkt, timer=timer)
+    bobs_tap = Tap(withdrawer=bob, upstream=bkt, state=Tap.State.CLOSED, timer=timer)
+    assert token.balance_of(alice) == 0
+    res = alices_tap.withdraw(1)
+    assert res == True
+    assert token.balance_of(alice) == 1
+    assert token.balance_of(bob) == 0
+    res = bobs_tap.withdraw(1)
+    assert res == False
+    assert token.balance_of(bob) == 0
+    bobs_tap.set_rate(100)
+
+def test_bucket_rate():
+    timer = Timer()
+    alice = Account()
+    token = ERC20Token()
+    bkt = Bucket(token=token)
+    token.mint(bkt, 1000000)
+    assert token.balance_of(bkt) == 1000000
+    alices_tap = Tap(withdrawer=alice, upstream=bkt, rate=100, timer=timer)
+    assert alices_tap.available == 0
+    timer.increment(1000)
+    alices_tap.update()
+    assert alices_tap.available == 100000
+    assert token.balance_of(alice) == 0
+    assert token.balance_of(bkt) == 1000000
+    res = alices_tap.withdraw(100000)
+    assert res == True
+    assert token.balance_of(alice) == 100000
+    assert token.balance_of(bkt) == 1000000 - 100000
+    res = alices_tap.withdraw(900000)
+    assert res == False
+    assert token.balance_of(alice) == 100000
+    assert token.balance_of(bkt) == 1000000 - 100000
+    timer.increment(2000)
+    res = alices_tap.withdraw(200000)
+    assert res == True
+    assert token.balance_of(alice) == 300000
+    assert token.balance_of(bkt) == 1000000 - 300000
+    res = alices_tap.withdraw(1)
+    assert res == False
+    assert token.balance_of(alice) == 300000
+    assert token.balance_of(bkt) == 1000000 - 300000
+
+def test_tap_states():
+    token = ERC20Token()
+    bkt = Bucket(token=token)
+    alice = Account()
+    timer = Timer()
+    alices_tap = Tap(withdrawer=alice, upstream=bkt, rate=100, timer=timer)
+    assert alices_tap.state == Tap.State.PARTIAL
+    alices_tap.close()
+    assert alices_tap.state == Tap.State.CLOSED
+    assert alices_tap.rate == 0
+    alices_tap.open()
+    assert alices_tap.state == Tap.State.OPEN
+    assert alices_tap.rate == sys.maxsize
+    alices_tap.set_rate(100)
+    assert alices_tap.state == Tap.State.PARTIAL
+    assert alices_tap.rate == 100
+
+def test_withdraw_all():
+    token = ERC20Token()
+    bkt = Bucket(token=token)
+    token.mint(bkt, 1000000000)
+    alice = Account()
+    timer = Timer()
+    alices_tap = Tap(withdrawer=alice, upstream=bkt, rate=100, timer=timer)
+    assert alices_tap.state == Tap.State.PARTIAL
+    timer.increment(seconds=10)
+    assert token.balance_of(alice) == 0
+    assert alices_tap.get_available() == 1000
+    res = alices_tap.withdraw_all()
+    assert token.balance_of(alice) == 1000
+    assert alices_tap.available == 0
+    assert alices_tap.get_available() == 0
+    assert token.balance_of(alices_tap) == 0
+
+
+
+
+
+
+
+
 
--- tokenomics.py
+++ tokenomics.py
@@ -1,5 +1,8 @@
 """The tokenomics package for token economy modeling"""
 import os
+import sys
+from enum import Enum
+from datetime import datetime, timedelta
 
 
 class Account:
@@ -8,6 +11,15 @@ class Account:
         if frm:
             self.owner = frm
         self.addr = "0x"+os.urandom(20).hex()
+
+class Timer:
+    def __init__(self, start = None):
+        if start == None:
+            self.time = datetime.now()
+        else:
+            self.time = start
+    def increment(self, seconds):
+        self.time += timedelta(seconds = seconds)
 
 
 class ERC20Token(Account):
@@ -19,7 +31,10 @@ class ERC20Token(Account):
 
     def balance_of(self, holder: Account):
         """balanceOf ERC-20 method re-implementation"""
-        return self.balances[holder.addr]
+        if holder.addr in self.balances:
+            return self.balances[holder.addr]
+        else:
+            return 0
 
     def mint(self, to: Account, tokens):
         """mints (creates new amount of) tokens for the given account"""
@@ -61,21 +76,65 @@ class Bucket(Account):
 
 
 class Tap(Account):
-    def __init__(self, upstream = None):
+    class State(Enum):
+        CLOSED = 0
+        PARTIAL = 1
+        OPEN = 2
+    def __init__(self, upstream = None, state = State.OPEN, rate = None, withdrawer = None, timer = None):
         super().__init__()
         self.upstream = upstream # upstream bucket
+        self.state = state
+        if rate != None:
+            self.rate = rate
+            self.state = self.State.PARTIAL
+        else:
+            self.rate = sys.maxsize
+        self.withdrawer = withdrawer
+        self.available = 0
+        if type(timer) == Timer:
+            self.timer = timer
+            self.available_updated = self.timer.time
+
+    def update(self):
+        curtime = self.timer.time
+        delta = curtime - self.available_updated
+        self.available_updated = curtime
+        delta_sec = delta.total_seconds()
+        self.available += delta_sec * self.rate
+
+    def get_available(self):
+        self.update()
+        return self.available
 
     def close(self):
-        pass
+        self.available = 0
+        self.available_updated = self.timer.time
+        self.state = self.State.CLOSED
+        self.rate = 0
 
     def open(self):
-        pass
+        self.state = self.State.OPEN
+        self.rate = sys.maxsize
 
     def set_rate(self, rate):
-        pass
+        self.update()
+        self.state = self.State.PARTIAL
+        self.rate = rate
 
     def withdraw(self, amount):
-        pass
+        if self.state == self.State.CLOSED:
+            return False
+        if self.state in [self.State.OPEN, self.State.PARTIAL]:
+            if self.state == self.State.PARTIAL:
+                self.update()
+                if amount > self.available:
+                    return False
+                else:
+                    self.available -= amount
+            self.upstream.token.transfer(self.upstream, self.withdrawer, amount)
+            return True
 
     def withdraw_all(self):
-        pass
+        self.update()
+        amount = self.available
+        self.withdraw(amount)
